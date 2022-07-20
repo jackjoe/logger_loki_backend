03006a3dcb5d7e4a2d9ced55f734db05b926ee3b
Jeroen Bourgois <jeroen@jackjoe.be>
2022-07-20 08:40:25 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LoggerLokiBackend.MixProject do
   def project do
     [
       app: :logger_loki_backend,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
