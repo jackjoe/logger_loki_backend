158d92bd995222b0332027a37ef8c3b29f4eb748
Jeroen Bourgois <jeroen@jackjoe.be>
2022-07-20 08:08:32 +0200

    chore: cleanup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LoggerLokiBackend.MixProject do
   def project do
     [
       app: :logger_loki_backend,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
